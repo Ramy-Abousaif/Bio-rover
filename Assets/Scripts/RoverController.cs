31e29dfba9c067f38b1fe06308c9c933d1587ff6
Ramy-Abousaif <ramydragolord@gmail.com>
2022-12-12 15:41:07 +0000

    Inflating/deflating + more sound effects

--- Assets/Scripts/RoverController.cs
+++ Assets/Scripts/RoverController.cs
@@ -7,12 +7,13 @@ public class RoverController : MonoBehaviour
 {
     private CinemachineImpulseSource impulse;
     private Rigidbody rb;
+    private SphereCollider sc;
     public Transform folloCam;
+    public GameObject draw;
 
     public LayerMask ground;
     private bool isGrounded = false;
     private bool landed = true;
-    private float groundOffset = 3f;
     private float coyoteTime = 0.2f;
     private float scanCooldown = 2.0f;
     private float scanTimer = 0.0f;
@@ -21,6 +22,13 @@ public class RoverController : MonoBehaviour
     private float speedMult = 4.0f;
     private float jumpForce = 500f;
     private Vector3 moveDir;
+
+    private float initialRadius = 1.5f;
+    private float targetRadius = 3f;
+    private Vector3 initialSize = new Vector3(3, 3, 3);
+    private Vector3 targetSize = new Vector3(6, 6, 6);
+    private float sizeShiftTime = 1.5f;
+    private bool isChangingSize = false;
 
     public GameObject scanner;
     private float landSpeed;
@@ -33,14 +41,17 @@ public class RoverController : MonoBehaviour
         Cursor.visible = false;
         impulse = GetComponent<CinemachineImpulseSource>();
         rb = GetComponent<Rigidbody>();
+        sc = GetComponent<SphereCollider>();
         scanTimer = scanCooldown;
+        sc.radius = initialRadius;
+        draw.transform.localScale = initialSize;
     }
 
     void Update()
     {
         scanTimer += Time.deltaTime;
         float currentSpeed = Mathf.Sqrt((rb.velocity.x * rb.velocity.x) + (rb.velocity.z * rb.velocity.z));
-        isGrounded = Physics.CheckSphere(new Vector3(transform.position.x, transform.position.y - groundOffset, transform.position.z), 0.1f, ground);
+        isGrounded = Physics.CheckSphere(new Vector3(transform.position.x, transform.position.y - sc.radius, transform.position.z), 0.1f, ground);
 
         if (isGrounded)
         {
@@ -63,11 +74,17 @@ public class RoverController : MonoBehaviour
             Instantiate(scanner, transform.position, Quaternion.identity);
             scanTimer = 0.0f;
         }
+
+        if (Input.GetKeyDown(KeyCode.E) && !isChangingSize)
+            StartCoroutine(ChangeSize());
     }
 
     void FixedUpdate()
     {
         if (rb == null)
+            return;
+
+        if (isChangingSize)
             return;
 
         rb.AddTorque(moveDir.normalized * roverSpeed * speedMult);
@@ -80,7 +97,8 @@ public class RoverController : MonoBehaviour
         if(landSpeed > 4f || prevSpeed > 10f)
             Shake();
 
-        AudioManager.Instance.PlayOneShotWithParameters("BallLand", transform);
+        if(!isChangingSize)
+            AudioManager.Instance.PlayOneShotWithParameters("BallLand", transform);
     }
 
     void Shake()
@@ -88,5 +106,36 @@ public class RoverController : MonoBehaviour
         impulse.m_DefaultVelocity *= Mathf.Clamp(landSpeed / 10f, 0f, 6f);
         impulse.GenerateImpulse();
         impulse.m_DefaultVelocity = new Vector3(-1f, -1f, -1f);
+    }
+
+    IEnumerator ChangeSize()
+    {
+        isChangingSize = true;
+        float time = 0f;
+
+        if (initialRadius > targetRadius)
+            AudioManager.Instance.PlayOneShotWithParameters("Deflate", transform);
+        else
+            AudioManager.Instance.PlayOneShotWithParameters("Inflate", transform);
+
+        while (time < sizeShiftTime)
+        {
+            float newRadius = Mathf.Lerp(initialRadius, targetRadius, time / sizeShiftTime);
+            Vector3 newSize = Vector3.Lerp(initialSize, targetSize, time / sizeShiftTime);
+            sc.radius = newRadius;
+            draw.transform.localScale = newSize;
+            time += Time.deltaTime;
+            yield return null;
+        }
+
+        Vector3 tempSize = initialSize;
+        initialSize = targetSize;
+        targetSize = tempSize;
+
+        float tempRadius = initialRadius;
+        initialRadius = targetRadius;
+        targetRadius = tempRadius;
+
+        isChangingSize = false;
     }
 }
