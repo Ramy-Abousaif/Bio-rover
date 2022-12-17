d46b10320f6cb2bc5d0d33a403ac952882db7a33
Ramy-Abousaif <ramydragolord@gmail.com>
2022-12-17 12:25:45 +0000

    Marimos now generate energy and detect direction/intensity/blocked light
    WIP

---
+++ Assets/Scripts/GenerateEnergy.cs
@@ -0,0 +1,53 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GenerateEnergy : MonoBehaviour
+{
+    public LayerMask lm;
+    private bool blocked = false;
+    private float photosynthesis;
+    private float rate = 2.0f;
+    private float energy = 0;
+    private Transform sunlight;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        sunlight = RenderSettings.sun.transform;
+    }
+
+    void Update()
+    {
+        photosynthesis = Vector3.Dot(transform.forward, -sunlight.forward);
+
+        RaycastHit hit;
+        if (Physics.Raycast(transform.position, -sunlight.forward, out hit, Mathf.Infinity, lm))
+        {
+            blocked = true;
+            /*
+            if (photosynthesis >= 0 && photosynthesis <= 0.5f)
+                Debug.Log(gameObject.name + " is facing towards the sun at a low intensity and is currently blocked by " + hit.transform.name + ". Energy = " + energy);
+            else if (photosynthesis > 0.5f)
+                Debug.Log(gameObject.name + " is facing towards the sun at a high intensity and is currently blocked by " + hit.transform.name + ". Energy = " + energy);
+            */
+        }
+        else
+        {
+            blocked = false;
+            /*
+            if (photosynthesis >= 0 && photosynthesis <= 0.5f)
+                Debug.Log(gameObject.name + " is facing towards the sun at a low intensity and is currently not blocked" + ". Energy = " + energy);
+            else if (photosynthesis > 0.5f)
+                Debug.Log(gameObject.name + " is facing towards the sun at a high intensity and is currently not blocked" + ". Energy = " + energy);
+            */
+        }
+
+        //Debug.DrawRay(transform.position, -sunlight.forward * 1000000000f, Color.yellow);
+
+        if (photosynthesis > 0 && !blocked)
+            energy += photosynthesis * (rate * Time.deltaTime);
+
+        energy = Mathf.Clamp(energy, 0, 10);
+    }
+}
