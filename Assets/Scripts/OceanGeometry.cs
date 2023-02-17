04fcf8e82cd00abc1a222ffc26d86eaf3530a16e
Ramy-Abousaif <ramydragolord@gmail.com>
2023-02-17 18:36:52 +0000

    Fixed infinite water

--- Assets/Scripts/OceanGeometry.cs
+++ Assets/Scripts/OceanGeometry.cs
@@ -3,19 +3,19 @@ using UnityEngine;
 
 public class OceanGeometry : MonoBehaviour
 {
-    [SerializeField]
     private Transform viewer;
     [SerializeField]
     private GameObject water;
-    private int count = 3;
+    private int count = 4;
 
     private void Start()
     {
+        viewer = Camera.main.transform;
         for (int i = 0; i < count; i++)
         {
             for (int j = 0; j < count; j++)
             {
-                Instantiate(water, new Vector3(transform.position.x + (i * 512) - 512, transform.position.y, transform.position.z + (j * 512) - 512), Quaternion.identity, transform);
+                Instantiate(water, new Vector3(transform.position.x + (i * 512) - (192 * count), transform.position.y, transform.position.z + (j * 512) - (192 * count)), Quaternion.identity, transform);
             }
         }
     }
